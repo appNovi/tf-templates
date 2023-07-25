bd25f1c36f7dc903925a5bffc2b7d8ca5d741309
Aaron Goleman <28195615+aarongoleman@users.noreply.github.com>
2023-07-25 15:28:34 -0500

    Added README file and significant changes to all aspects of the tf module.

--- integration-modules/aws-integration-module/outputs.tf
+++ integration-modules/aws-integration-module/outputs.tf
@@ -1,12 +1,4 @@
-locals {
-  credentials_content = <<EOF
-[default]
-aws_access_key_id     = ${data.aws_iam_access_key.new_user_access_key.access_key}
-aws_secret_access_key = ${data.aws_iam_access_key.new_user_access_key.secret_access_key}
-EOF
-}
-
-resource "local_file" "credentials_file" {
-  sensitive_content = local.credentials_content
-  filename = "credentials.txt"
+output "integration_role_arn" {
+  description = "Created ARN to be used in appNovi Integration"
+  value = aws_iam_role.app_novi_aws_integration_role.arn
 }
